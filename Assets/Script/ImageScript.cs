dbe23fd26080b2b8c663f02cac7fe58eddf63539
DagnisSkola <dagnis.janeks@sk.lvt.lv>
2024-05-15 09:24:59 +0300

    3 lekcija

--- Assets/Script/ImageScript.cs
+++ Assets/Script/ImageScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageScript : MonoBehaviour
 {
@@ -8,10 +10,16 @@ public class ImageScript : MonoBehaviour
     public GameObject teddy;
     public GameObject lady;
     public GameObject car;
+    public GameObject leftToggle;
+    public GameObject rightToggle;
+    public Sprite[] spriteArray;
+    public GameObject imageHolder;
 
     public void showBean(bool value)
     {
         bean.SetActive(value);
+        leftToggle.GetComponent<Toggle>().interactable = value;
+        rightToggle.GetComponent<Toggle>().interactable = value;
     }
 
     public void showTeddy(bool value)
@@ -27,5 +35,20 @@ public class ImageScript : MonoBehaviour
     public void showCar(bool value)
     {
         car.SetActive(value);
+    }
+
+    public void toLeft()
+    {
+        bean.transform.localScale = new Vector2(1, 1);
+    }
+
+    public void toRight()
+    {
+        bean.transform.localScale = new Vector2(-1, 1);
+    }
+
+    public void changeImage(int index)
+    {
+        imageHolder.GetComponent<Image>().sprite = spriteArray[index];
     }
 }
