0c46e5244ee3eb1037addf75180613c548b9f246
DagnisSkola <dagnis.janeks@sk.lvt.lv>
2024-05-15 10:41:54 +0300

    4 lekcija

--- Assets/Script/ImageScript.cs
+++ Assets/Script/ImageScript.cs
@@ -14,6 +14,8 @@ public class ImageScript : MonoBehaviour
     public GameObject rightToggle;
     public Sprite[] spriteArray;
     public GameObject imageHolder;
+    public GameObject sizeSlider;
+    public GameObject rotationSlider;
 
     public void showBean(bool value)
     {
@@ -51,4 +53,17 @@ public class ImageScript : MonoBehaviour
     {
         imageHolder.GetComponent<Image>().sprite = spriteArray[index];
     }
+
+    public void changeSize()
+    {
+        float size = sizeSlider.GetComponent<Slider>().value;
+        imageHolder.transform.localScale = new Vector2(1F * size, 1F * size);
+    }
+
+    public void changeRotation()
+    {
+        float rotation = rotationSlider.GetComponent<Slider>().value;
+        imageHolder.transform.localRotation = Quaternion.Euler(0, 0, rotation*360);
+    }
+
 }
